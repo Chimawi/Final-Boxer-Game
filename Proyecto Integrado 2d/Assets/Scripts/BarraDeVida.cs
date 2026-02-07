0f7248a2bcdcce6fcc693850e2c1ea92c768350b
luciallanes12-design <luciallanes12@gmail.com>
2026-02-07 10:09:11 +0100

    barra de vida y ring

---
+++ Proyecto Integrado 2d/Assets/Scripts/BarraDeVida.cs
@@ -0,0 +1,17 @@
+using Unity.Hierarchy;
+using UnityEngine;
+using UnityEngine.UI;
+public class BarraDeVida : MonoBehaviour
+{
+    
+    public Image barraDeVida;
+
+    public float vidaActual;
+
+    public float vidaMaxima;
+
+    void Update()
+    {
+        barraDeVida.fillAmount = vidaActual / vidaMaxima;
+    }
+}
